19aab21c7676c40c02f42d3ac1cf0b146042b406
Irving Vanegas <dark-metal_master@hotmail.com>
2023-10-11 15:52:38 -0600

    .

--- src/components/includes/Navigationbar.js
+++ src/components/includes/Navigationbar.js
@@ -12,7 +12,15 @@ function Navigationbar() {
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand as={Link} to="/" >Navbar</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/" >
+            <img
+                src="/imgages/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/" >Principal</Nav.Link>
           </Nav>
