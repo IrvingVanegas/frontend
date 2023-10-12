5b751d67c754991cce6ca7bf66b190d7bdcdc649
Irving Vanegas <dark-metal_master@hotmail.com>
2023-10-12 13:34:51 -0600

    .

--- src/components/includes/Navigationbar.js
+++ src/components/includes/Navigationbar.js
@@ -3,7 +3,6 @@ import Home from "../pages/Home";
 import Medicion from "../pages/Medicion";
 import { Container } from "react-bootstrap";
 import { Navbar } from "react-bootstrap";
-import { Nav } from "react-bootstrap";
 import { Routes, Route, Link } from 'react-router-dom';
 
 
@@ -13,17 +12,9 @@ function Navigationbar() {
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand as={Link} to="/" >
-            <img
-                src="/imgages/logo.svg"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-                alt="React Bootstrap logo"
-            />
+            <img src="../images/logo.png" width="40px" height="40px" alt="home"/> {' '}
+            Analisis de Datos de Agrovoltaica
           </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" >Principal</Nav.Link>
-          </Nav>
         </Container>
       </Navbar>
       <div>
